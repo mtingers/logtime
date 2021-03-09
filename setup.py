b73ce9b78a62f3c0c5a35507bdaaaabc33ad442b
Matthew Ingersoll <matth@mtingers.com>
2021-03-09 10:17:39 -0800

    Fix setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     packages=['logtime',],
     license='BSD 2-Clause License',
     long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     url='https://github.com/mtingers/logtime',
     install_requires=[
     ],
